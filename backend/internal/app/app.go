dcc256d540ee8b59a222dd1eda47f12e058ac80e
douglas_barraza <douglas.barraza@externos-cl.cencosud.com>
2025-07-04 08:41:54 +0100

    initial routes

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -5,18 +5,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"unimatch-back/internal/api"
 )
 
 
 type Application struct {
 	Logger *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "APP: ", log.Ldate| log.Ltime | log.Lshortfile)
 
+
+	// our stores will go here
+
+
+	// our handlers will go here
+
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
 		Logger: logger,
+		WorkoutHandler: workoutHandler,
 	}
 
 	return app, nil
--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"unimatch-back/app"
+	"unimatch-back/internal/app"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -10,6 +10,10 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/health", app.HealthCheck)
+	// Define routes for workout-related endpoints
+	r.Get("/workouts", app.WorkoutHandler.HandleGetWorkoutByID) // This should
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 
 	return r
 }
